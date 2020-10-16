959ab84888b88bf2d52109c1b9edcc67591ba25c
Zhaks <jansultan_141096@mail.ru>
2020-10-16 16:20:31 +0600

    new commit 16.10.2020 16-20

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import marked from 'marked'
 import VueRouter from 'vue-router'
 import router from './router/index'
 import store from './store/index'
+import VueMask from 'v-mask'
+import ApiPlugin from './plugins/api'
+import LoadPlugin from './plugins/load'
+import VModal from 'vue-js-modal'
+
+import 'material-design-icons-iconfont'
 
 // Vue.config.productionTip = false
 import '@/assets/styles/main.scss';
@@ -13,6 +19,10 @@ import '@/assets/styles/main.scss';
 Vue.use(SimplePlugin)
 Vue.filter('marked', marked)
 Vue.use(VueRouter)
+Vue.use(VueMask)
+Vue.use(ApiPlugin)
+Vue.use(LoadPlugin)
+Vue.use(VModal)
 
 new Vue({
     render: h => h(App),
